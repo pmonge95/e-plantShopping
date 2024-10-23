70a3d7cc1dcca4d77cdc51c210f2b764facc1026
pmonge95 <pmonge95@gmail.com>
2024-10-23 11:56:13 -0400

    Initial arrangements

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,35 @@ import AboutUs from './AboutUs';
 
 function App() {
   
-  const [showProductList, setShowProductList] = useState(false);
+    const [showProductList, setShowProductList] = useState(false);
 
-  const handleGetStartedClick = () => {
-    setShowProductList(true);
-  };
+    const handleGetStartedClick = () => {
+        setShowProductList(true);
+    };
 
-  return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
-
-      </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList />
-      </div>
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+                <div className="background-image"></div>
+                <div className="content">
+                    <div className="landing_content">
+                        <h1>Welcome To Paradise Nursery</h1>
+                        <div className="divider"></div>
+                        <p>Where Green Meets Serenity</p>
+                        <button className="get-started-button" onClick={handleGetStartedClick}>
+                            Get Started
+                        </button>
+                    </div>
+                    <div className="aboutus_container">
+                        <AboutUs/>
+                    </div>
+                </div>
+            </div>
+            <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+                <ProductList />
+            </div>
+        </div>
+    );
 }
 
 export default App;
