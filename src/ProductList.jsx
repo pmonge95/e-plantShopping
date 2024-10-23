ee22fb6d9a48db101cb3ecbd1485854c983ded47
pmonge95 <pmonge95@gmail.com>
2024-10-23 15:47:56 -0400

    Added next steps

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ProductList() {
 
@@ -11,6 +11,10 @@ function ProductList() {
     const [showCart, setShowCart] = useState(false); 
 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const cartItems = useSelector((state) => state.cart.items);
 
     const plantsArray = [
         {
@@ -257,9 +261,8 @@ function ProductList() {
     const handleContinueShopping = (e) => {
         e.preventDefault();
         setShowCart(false);
+        setAddedToCart({});
     };
-
-    const [addedToCart, setAddedToCart] = useState({});
 
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
@@ -268,6 +271,16 @@ function ProductList() {
            [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
         }));
     };
+
+    const calculateTotalQuantity = () => {
+        return cartItems ? cartItems.reduce((total, item) => total + item.quantity, 0): 0;
+    };
+    
+    const [totalQuantity, setTotalQuantity] = useState(calculateTotalQuantity());
+    useEffect(() => {
+        setTotalQuantity(calculateTotalQuantity());
+    }, [cartItems]);
+    
 
     return (
         <div>
@@ -285,7 +298,19 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> 
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                    <rect width="156" height="156" fill="none"></rect>
+                                    <circle cx="80" cy="216" r="12"></circle>
+                                    <circle cx="184" cy="216" r="12"></circle>
+                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                                </svg>
+                                <div className='cart_quantity_count'>{totalQuantity}</div>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart? (
@@ -300,7 +325,10 @@ function ProductList() {
                                         <div className="product-title">{plant.name}</div>
                                         <div className="product-description">{plant.description}</div>
                                         <div className="product-cost">{plant.cost}</div>
-                                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                        <button  className="product-button" 
+                                        onClick={() => handleAddToCart(plant)}
+                                        >
+                                            Add to Cart</button>
                                     </div>
                                 ))}
                             </div>
